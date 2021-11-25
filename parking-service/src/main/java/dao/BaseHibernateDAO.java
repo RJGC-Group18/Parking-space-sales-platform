51af13bf5ca7d2fcd4c5c1938470695347029984
sxxcn <2403600964@qq.com>
2021-11-25 22:44:33 +0800

    基础DAO类

--- parking-service/src/main/java/dao/BaseHibernateDAO.java
+++ parking-service/src/main/java/dao/BaseHibernateDAO.java
@@ -2,7 +2,8 @@ package dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-//基础DAO类，用来定义获取session的方法，写DAO务必继承该类
+//基础DAO类，用来定义获取session的方法
+//写DAO务必继承该类，用getSession获取session来对数据库进行操作
 public class BaseHibernateDAO {
 	private SessionFactory sessionFactory;
 	public Session getSession()
